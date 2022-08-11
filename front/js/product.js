d31a31b45f4cd13f3ab7477c33119d1f965cac00
Hans Schmit <hans.schmit@hotmail.fr>
2022-08-11 16:48:48 +0200

    Corrections/refactoring and added check functions

--- front/js/product.js
+++ front/js/product.js
@@ -1,50 +1,83 @@
+/**
+ * Start - Function to get the product with the right id from the api
+ */
+const start = () => {
+    fetch(`http://localhost:3000/api/products/${productId}`)
+        .then(res => res.json())
+        .then(product => {
+            insertIntoHtml(product)
+        })
+        .catch(err => {
+            console.log(`An error has occured : ${err}`)
+        })
+}
+
 class Product {
     constructor(id, color, quantity) {
-        this.id = id;
-        this.color = color;
-        this.quantity = quantity;
+        this.id = id
+        this.color = color
+        this.quantity = quantity
     }
 }
 
-//extract the product id from the url
-let url = new URL(window.location.href);
-let productId = url.searchParams.get("id");
+/**
+ * extract the product id from the url
+ */
+let url = new URL(window.location.href)
+let productId = url.searchParams.get("id")
 
-//insert the product datas in the DOM
+/**
+ * insert the product datas in the DOM
+ */
 const insertIntoHtml = (product) => {
-    let colorsHtml = "";
-    document.title = product.name;
-    document.getElementsByClassName("item__img")[0].innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
-    document.getElementById("title").innerText = product.name;
-    document.getElementById("price").innerText = product.price;
-    document.getElementById("description").innerText = product.description;
+    let colorsHtml = ""
+    document.title = product.name
+    document.getElementsByClassName("item__img")[0].innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
+    document.getElementById("title").innerText = product.name
+    document.getElementById("price").innerText = product.price
+    document.getElementById("description").innerText = product.description
+
     for (let color of product.colors) {
-        colorsHtml += `<option value="${color}">${color}</option>`;
+        colorsHtml += `<option value="${color}">${color}</option>`
     }
-    document.getElementById("colors").innerHTML += colorsHtml;
+    document.getElementById("colors").innerHTML += colorsHtml
+
+    //document.getElementById("colors").insertAdjacentHTML('beforeend', product.colors.map(color => `<option value="${color}">${color}</option>`).join(' '))
+
+    startListener()
 }
 
-//add the product (id, color and quantity) to the cart
-const addProduct = (event) => {
-    event.stopPropagation();
-    let color = document.getElementById("colors").value;
-    let quantity = document.getElementById("quantity").value;
-    let addedProduct = new Product(productId, color, quantity);
-    console.log(addedProduct);
+/**
+ * Set listener to handle add product action
+ */
+const startListener = () => {
+    document.getElementById("addToCart").addEventListener("click", addProduct)
 }
 
-fetch(`http://localhost:3000/api/products/${productId}`) //get the product with the right id from the api
-    .then(function (res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-    .then(function (product) {
-        insertIntoHtml(product);
-    })
-    .catch(function (err) {
-        console.log(`An error has occured : ${err}`);
-    });
+/**
+ * add the product (id, color and quantity) to the cart
+ */
+const addProduct = () => {
+    let color = document.getElementById("colors").value
+    let quantity = Number(document.getElementById("quantity").value)
+    if (checkColor(color) && checkQuantity(quantity)) {
+        let addedProduct = new Product(productId, color, quantity)
+        console.log(addedProduct)
+    }
+}
+/**
+ * checkColor - check if a color is selected
+ * @param {*} color 
+ * @returns 
+ */
+const checkColor = (color) => Boolean(color != "")
 
+/**
+ * checkQuantity - check if the quantity is a number between 1 and 100 included
+ * @param {*} quantity 
+ * @returns 
+ */
+const checkQuantity = (quantity) => Boolean(1 <= quantity && quantity <= 100)
 
-document.getElementById("addToCart").addEventListener("click", addProduct);
+// Wait for DOM release
+window.addEventListener('load', start())
--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,25 @@
+/**
+ * Start - Function to get all product from API
+ */
+const start = () => {
+    fetch("http://localhost:3000/api/products/")
+        .then(res => res.json())
+        .then(data => {
+            productsBrowse(data)
+        })
+        .catch(err => {
+            document.getElementById("items").innerText = `An error has occured : ${err}`
+        })
+}
+
+/**
+ * productsBrowse - browse each product in the array and add the html code with correct product infos in a variable htmlContent
+ * 
+ * @param {Array} - Api response (Array of object)
+ */
 const productsBrowse = (products) => {
-    let htmlContent = "";
-    //browse each product in the array and add the html code with correct product infos in a variable htmlContent
+    let htmlContent = ""
+
     for (let product of products) {
         htmlContent += `<a href="./product.html?id=${product._id}">
                 <article>
@@ -8,22 +27,12 @@ const productsBrowse = (products) => {
                     <h3 class="productName">${product.name}</h3>
                     <p class="productDescription">${product.description}</p>
                 </article>
-            </a>`;
+            </a>`
     }
     //add the code for all products in the DOM
-    document.getElementById("items").innerHTML = htmlContent;
+    document.getElementById("items").innerHTML = htmlContent
 }
 
 
-fetch("http://localhost:3000/api/products/") // getting all products from api
-    .then(function (res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-    .then(function (value) {
-        productsBrowse(value);
-    })
-    .catch(function (err) {
-        document.getElementById("items").innerText = `An error has occured : ${err}`;
-    });
+// Wait for DOM release
+window.addEventListener('load', start())
