a70661cb10f432635201741d7f8763288f16d681
Hans Schmit <hans.schmit@hotmail.fr>
2022-08-18 16:46:48 +0200

    Reworkes all functions and added some

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,45 +1,64 @@
+/**
+ * start - get the cart and the products then call the function to add the elements to the page
+ */
 const start = async () => {
-    let cart = JSON.parse(localStorage.cart)
-    try {
-        let datas = await loopThroughCart(cart)
-        console.log(datas)
-        addToPage(datas)
-    }
-    catch (err) {
-        console.log(err)
-    }
+  let cart = JSON.parse(localStorage.cart)
+  try {
+    let products = await fetch("http://localhost:3000/api/products/")
+      .then(res => res.json())
+      .then(data => {
+        return (data)
+      })
+      .catch(err => {
+        console.error(`An error has occured : ${err}`)
+      })
+    let datas = loopThroughCart(cart, products)
+    addToPage(datas)
+    startListener()
+  }
+  catch (err) {
+    console.log(err)
+  }
 
 }
 
-const loopThroughCart = (cart) => {
-    let htmlCode = ""
-    let totalArticles = 0
-    let totalPrice = 0
-    cart.forEach(article => {
-        fetch(`http://localhost:3000/api/products/${article.id}`)
-            .then(res => res.json())
-            .then(product => {
-                htmlCode += String(addToHtml(article, product))
-                totalArticles += Number(article.quantity)
-                totalPrice += Number(product.price * article.quantity)
-            })
-            .catch(err => {
-                console.log(`An error has occured : ${err}`)
-            })
-    })
-    return [htmlCode, totalArticles, totalPrice]
+/**
+ * loopThroughCart - go through the cart and  for each products call addToHtm, increase the total quantity and the total price
+ * @param {*} cart 
+ * @param {*} products 
+ * @returns 
+ */
+const loopThroughCart = (cart, products) => {
+  let datas = { htmlCode: "", totalArticles: 0, totalPrice: 0 }
+  cart.forEach(article => {
+    let product = products.find((aProduct) => aProduct._id == article.id)
+    datas.htmlCode += addToHtml(article, product)
+    datas.totalArticles += Number(article.quantity)
+    datas.totalPrice += Number(product.price * article.quantity)
+  })
+  return datas
 }
 
-const addToPage = ([htmlCode, totalArticles, totalPrice]) => {
-    document.querySelector("#cart__items").innerHTML = htmlCode
-    document.querySelector("#totalQuantity").innertext = totalArticles
-    document.querySelector("#totalPrice").innertext = totalPrice
+/**
+ * addToPage - add the cart products, total of articles and total price to the page
+ * @param {*} datas 
+ */
+const addToPage = (datas) => {
+  document.querySelector("#cart__items").innerHTML = datas.htmlCode
+  document.querySelector("#totalQuantity").innerText = datas.totalArticles.toString()
+  document.querySelector("#totalPrice").innerText = datas.totalPrice.toString()
 }
 
+/**
+ * addToHtml - return the html code for a product
+ * @param {*} article 
+ * @param {*} product 
+ * @returns 
+ */
 const addToHtml = (article, product) => {
-    return `<article class="cart__item" data-id="${product._id}" data-color="${article.color}">
+  return `<article class="cart__item" data-id="${product._id}" data-color="${article.color}">
     <div class="cart__item__img">
-      <img src="../images/${product.imageUrl}" alt="${product.altTxt}">
+      <img src="${product.imageUrl}" alt="${product.altTxt}">
     </div>
     <div class="cart__item__content">
       <div class="cart__item__content__description">
@@ -60,5 +79,25 @@ const addToHtml = (article, product) => {
   </article>`
 }
 
+
+const startListener = () => {
+  let quantityList = Array.from(document.querySelectorAll(".itemQuantity"))
+  let deleteList = Array.from(document.querySelectorAll(".deleteItem"))
+  quantityList.forEach(selector => {
+    selector.addEventListener("onChange", newQuantity())
+  })
+  deleteList.forEach(selector => {
+    selector.addEventListener("click", deletItem())
+  })
+
+}
+
+const newQuantity = () => {
+
+}
+
+const deletItem = () => {
+
+}
 // Wait for DOM release
 window.addEventListener('load', start())
